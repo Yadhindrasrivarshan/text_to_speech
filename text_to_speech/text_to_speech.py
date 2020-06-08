d20e679ce3e262e9441f074d32799849d6b1bc30
Yadhindrasrivarshan <51809910+Yadhindrasrivarshan@users.noreply.github.com>
2020-06-08 15:41:54 +0530

    Update text_to_speech.py

--- text_to_speech/text_to_speech.py
+++ text_to_speech/text_to_speech.py
@@ -1,6 +1,7 @@
 import os
 import io
 import pygame
+import threading
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import ttk
@@ -13,36 +14,45 @@ class Translator_App(object):
         frame = Frame(master)
         frame.grid()
         tabControl = ttk.Notebook(root)
-        tabControl.configure(width=305, height=200)
+        tabControl.configure(width=395, height=200)
 
         self.translate_tab = ttk.Frame(tabControl)
         tabControl.add(self.translate_tab, text="Translate")
         tabControl.grid()
+        self.translate_tab.grid_propagate(0)
+
         self.about_tab = ttk.Frame(tabControl)
         tabControl.add(self.about_tab, text="About")
         tabControl.grid()
 
         self.speak_it = BooleanVar()
         self.language = StringVar()
-        self.languages = [
-            'en',
-            'ar',
-            'bg',
-            'bs',
-            'cs',
-            'da',
-            'de',
-            'el',
-            'en',
-            'es',
-            'ga',
-            'ru',
-        ]
+        self.languages = {
+            'Arabic': 'ar', 'Belarusian': 'be',
+            'Bulgarian': 'bg', 'Bosnian': 'bs',
+            'Czech': 'cs', 'Danish': 'da',
+            'German': 'de', 'Greek': 'el',
+            'English': 'en', 'Spanish': 'es',
+            'Persian': 'fa', 'Finnish': 'fi',
+            'French': 'fr', 'Irish': 'ga',
+            'Hebrew': 'he', 'Hindi': 'hi',
+            'Hungarian': 'hu', 'Armenian': 'hy',
+            'Indonesian': 'id', 'Italian': 'it',
+            'Japanese': 'ja', 'Korean': 'ko',
+            'Kurdish': 'ku', 'Latin': 'la',
+            'Lithuanian': 'lt', 'Latvian': 'lv',
+            'Dutch': 'nl', 'Norwegian': 'no',
+            'Polish': 'pl', 'Portuguese': 'pt',
+            'Romanian': 'ro', 'Russian': 'ru',
+            'Somalia': 'so', 'Albanian': 'sq',
+            'Serbian': 'sr', 'Swedish': 'sv',
+            'Swahili': 'sw', 'Turkish': 'tr',
+            'Vietnamese': 'vi'}
         self.translate_page()
         self.about_page()
 
     def translate_page(self):
-        self.top_label = Label(self.translate_tab, text="Enter Word You Wish To Translate:")
+        self.top_label = Label(self.translate_tab, text="Enter Sentence To Translate:")
         self.top_label.grid(column=0, row=0)
 
         self.word_entry = Entry(self.translate_tab, width=48)
@@ -50,29 +60,32 @@ class Translator_App(object):
 
         self.language_label = Label(self.translate_tab, text="Language: ")
         self.language_label.grid(column=0, row=2, pady=5)
-        self.language_menu = OptionMenu(self.translate_tab, self.language, *self.languages)
+
+        self.language_menu = ttk.Combobox(self.translate_tab, values=[*self.languages.keys()])
         self.language_menu.grid(column=1, row=2)
-        
-        self.translate_button = Button(self.translate_tab, text="Translate!", command=self.translate_func,bg='blue')
+        self.language_menu.current(0)
+
+        self.translate_button = Button(self.translate_tab, text="Translate!", command=self.translate_func)
         self.translate_button.grid(column=0, row=3, pady=15)
 
-        self.speek_check = Checkbutton(self.translate_tab, text="Say It!", variable=self.speak_it,bg='red')
-        self.speek_check.grid(column=1, row=3)
+        self.speak_check = Checkbutton(self.translate_tab, text="Say It!", variable=self.speak_it)
+        self.speak_check.grid(column=1, row=3)
 
         self.result = Label(self.translate_tab, text="")
-        self.result.grid(column=0, row=4)
-        
+        self.result.grid(column=0, row=4, columnspan=3)
+
     def about_page(self):
         pass
 
     def translate_func(self):
         word = self.word_entry.get()
-        language = self.language.get()
+        language = self.languages.get(self.language_menu.get())
         translator = Translator(service_urls=["translate.google.com"])
         translation = translator.translate(word, dest=language)
+        speak_thread = threading.Thread(target=self.speak, args=(translation.text, language))
         self.result.configure(text=translation.text)
         if self.speak_it.get():
-            self.speak(translation.text, language)
+            speak_thread.start()
 
     def speak(self, text, language):
         with io.BytesIO() as file:
@@ -84,9 +97,10 @@ class Translator_App(object):
             while pygame.mixer.music.get_busy():
                 continue
 
+
 if __name__ == '__main__':
     root = Tk()
-    root.title("Translator GUI app")
-    root.geometry("317x235")
+    root.title("Translator")
+    root.geometry("400x235")
     Translator_App(root)
     root.mainloop()
